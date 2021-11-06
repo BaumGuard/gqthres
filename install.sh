8c42c54cba916f360b1ef59cbd33bd1848e26224
BaumGuard <44273713+BaumGuard@users.noreply.github.com>
2021-11-06 13:48:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+cd src
+javac -cp jsoup-1.13.1.jar gqthres.java
+jar cfm gqthres.jar manifest *.class
+cp gqthres.jar /usr/lib/gqthres
+
+cd..
+
 mkdir -p /usr/lib/gqthres
 
-cp gqthres.jar /usr/lib/gqthres
+
 cp gqthres /usr/bin/gqthres
 cp gqthres.conf /etc
 cp gqthres.desktop /usr/share/applications
