1b023c371148dc7f231ad05c7316c577bad18872
BaumGuard <44273713+BaumGuard@users.noreply.github.com>
2023-03-07 23:14:12 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+echo "For which user do you want to uninstall gqthres?:"
+read username
+
 rm -r /usr/lib/gqthres
 rm /usr/bin/gqthres
 rm /etc/gqthres.conf
 rm /usr/share/applications/gqthres.desktop
 rm /usr/share/pixmaps/gqthres.png
+rm -r /home/$username/.cache/gqthres
